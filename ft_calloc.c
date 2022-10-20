2ae8beb070af8f04136472e2dec226b3e2476b38
Mohamed Bousoufi <mbousouf@e1r8p13.1337.ma>
2022-10-20 18:45:36 +0100

    mohim rak 3rf

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mbousouf <mbousouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:43:30 by mbousouf          #+#    #+#             */
-/*   Updated: 2022/10/20 18:34:35 by mbousouf         ###   ########.fr       */
+/*   Updated: 2022/10/20 18:42:25 by mbousouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,16 @@ void * ft_calloc(size_t count ,size_t size)
   size_t i =0;
     char *p;
         p = malloc(Max);
+	if(p <= 0)
+	{
+		return NULL;
+	}
     while(i < Max)
     {
         p[i] = '\0';
         i++;
     }
-    return (p);
+    return ((void*)p);
 } 
 
 
